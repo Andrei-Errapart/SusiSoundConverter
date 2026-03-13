7dad3083caf74cb6b9b2a6d689aebe261a5ce073
Andrei Errapart <andrei@errapartengineering.com>
2026-03-13 18:24:56 +0100

    Renamed src/main.zig to src/soundconvert.zig

--- build.zig
+++ build.zig
@@ -7,7 +7,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "convertsound",
         .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
+            .root_source_file = b.path("src/convertsound.zig"),
             .target = target,
             .optimize = optimize,
         }),
